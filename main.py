3df16c56417b7f6a6c241fdb50b0414d52aeddd8
matte54 <king_matthew@hotmail.com>
2021-08-24 21:23:12 +0200

    1

--- main.py
+++ main.py
@@ -1,64 +1,72 @@
 import pygame
 import random
+import sys
+import string
 import numpy as np
-from noisemap import generatenoise
+from system import Animal
 
 #resolution constants
 WINDOWWIDTH = 1020
 WINDOWHEIGHT = 820
 
+#init stuff
 pygame.display.set_caption("Ecosystem")
 WIN = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 FPS = 30
 
+class Animal():
+    def __init__(self):
+        self.moveSpeed = 0.5
+        self.awareness = 0.4
+        self.health = 1.0
+        self.hunger = 0.0
+        #self.thirst = 0.0
+        self.repoUrge = 0.0
+        self.pregnancy = 0.0
+        self.pregnant = False
+        self.maturity = 0.0
+        self.sex = random.randint(0,1) #0=female 1=male
+        self.name = f"bunny{''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))}"
+        if self.sex:
+            i = "male"
+            self.animal_surface = pygame.image.load('./assets/bunnymale.png').convert_alpha()
+        else:
+            i = "female"
+            self.animal_surface = pygame.image.load('./assets/bunnyfemale.png').convert_alpha()
+        print(f'created animal {i} {self.name}')
+
+        randomWidth = random.randint(0, 1020)
+        randomHeight = random.randint(0, 820)
+        self.animal_surface = pygame.transform.scale(self.animal_surface, (20, 20))
+        self.animal_rect = self.animal_surface.get_rect(topleft = (randomWidth, randomHeight))
+        WIN.blit(self.animal_surface, (self.animal_rect))
+
+
+
+
 def spawnBackground():
-    WIN.fill((0, 0, 255))
-    pygame.display.update()
-
-def spawnLand():
-    matrix = generatenoise()
-    x = 0
-    y = 0
-    for i in range(matrix.shape[0]):
-        x = 0
-        y += 10
-        for j in range(matrix.shape[1]):
-            x += 10
-            if matrix[i][j] == 1:
-                r = pygame.draw.rect(WIN,(0, 250, 0),(x, y, 10, 10))
-                land = pygame.Rect.union(r, r)
-    pygame.display.update()
-    return(land)
-
-def spawnBushes(number, land):
-    TREE_IMG = pygame.image.load("./assets/tree.png")
-    for i in range(number):
-        scale = random.randint(10,20)
-        rotation = random.randint(0, 360)
-        tree = pygame.transform.rotate(pygame.transform.scale(TREE_IMG, (scale, scale)), rotation)
-        x = random.randint(0, WINDOWWIDTH)
-        y = random.randint(0, WINDOWHEIGHT)
-        if land.collidepoint(x, y) == False:
-            WIN.blit(tree, (x, y))
-
+    WIN.fill((0, 250, 0))
     pygame.display.update()
 
 def main():
     clock = pygame.time.Clock()
     run = True
     spawnBackground()
-    land = spawnLand()
-    #spawnBushes(100, land)
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP:
-                spawnBackground()
-                spawnLand()
+                x = Animal()
+
+
 
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+                exit()
+
+
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
